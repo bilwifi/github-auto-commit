ded0936bd8e0f1ba4c84ecf47db38380894f3574
Luccin Masirika <luccinmasirika@gmail.com>
2019-12-20 17:58:26 +0100

    2019-12-20T17:58:26+01:00

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const makeCommit = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
-    .subtract(1, 'y')
+    .subtract(2, 'y')
     .add(1, 'd')
     .add(x, 'w')
     .add(y, 'd')
