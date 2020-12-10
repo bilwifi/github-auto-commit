b589a17fd6f1ebcace3cd1b8acbefd46f02dcb36
Luccin Masirika <luccinmasirika@gmail.com>
2020-12-10 01:03:51 +0100

    2020-12-10T01:03:51+01:00

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const FILE_PATH = './data.json';
 
 const makeCommit = async (n) => {
   if (n % 50 === 0 || n === 0) {
-    await simpleGit().push('origin', 'master');
+    await simpleGit().push('public', 'master');
     console.log('💅 50 COMMITS PUSHED ON GITHUB 🥰');
   }
   const x = random.int(0, 54);
